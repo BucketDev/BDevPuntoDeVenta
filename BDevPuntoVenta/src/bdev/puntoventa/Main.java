a8b206f7cd5685407ab6ae31ce516d7507367047
vanfigo <rodrigo.loyola@live.com>
2016-01-10 16:33:57 -0600

    Creación de la clase Main

---
+++ BDevPuntoVenta/src/bdev/puntoventa/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bdev.puntoventa;
+
+/**
+ * @author rodrigo
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
