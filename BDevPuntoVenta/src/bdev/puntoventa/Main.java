7bcf89bbc5402c6d5db43daf90a15a060ccbe6ec
vanfigo <rodrigo.loyola@live.com>
2016-01-24 13:43:11 -0600

    Se extiende la clase main para convertirla en una aplicación JavaFX

--- BDevPuntoVenta/src/bdev/puntoventa/Main.java
+++ BDevPuntoVenta/src/bdev/puntoventa/Main.java
@@ -3,18 +3,26 @@
  */
 package bdev.puntoventa;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * @author rodrigo
  *
  */
-public class Main {
+public class Main extends Application {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
